463001dc06e6ac1b3bbc08fd6ace0829b6ef96de
jay kruer <j@dank.systems>
2025-08-31 09:35:32 -0700

    fix

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ uv venv
 source .venv/bin/activate
 uv pip install numpy tiktoken
 uv pip install -e .
-BEAM=5 PYTHONPATH=. sudo -E python3 examples/llm.c/train_gpt2.py --batch_size 64 --sequence_length 256
+BEAM=5 PYTHONPATH=. python3 examples/llm.c/train_gpt2.py --batch_size 64 --sequence_length 256
