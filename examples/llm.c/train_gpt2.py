ac6cf57212082c31191ba87151ac8f29ee796275
jay kruer <j@dank.systems>
2025-08-31 09:33:06 -0700

    tweak llmm.c config for nanogpt

--- examples/llm.c/train_gpt2.py
+++ examples/llm.c/train_gpt2.py
@@ -121,16 +121,16 @@ if __name__ == "__main__":
 
   parser = argparse.ArgumentParser()
   parser.add_argument("--num_iterations", type=int, default=10, help="number of iterations to run")
-  parser.add_argument("--batch_size", type=int, default=4, help="batch size")
-  parser.add_argument("--sequence_length", type=int, default=64, help="sequence length")
+  parser.add_argument("--batch_size", type=int, default=64, help="batch size")
+  parser.add_argument("--sequence_length", type=int, default=256, help="sequence length")
   parser.add_argument("--skip_test", action="store_true", help="skip test")
   parser.add_argument("--gpus", type=int, default=1, help="sequence length")
   args = parser.parse_args()
   B, T = args.batch_size, args.sequence_length
   assert 1 <= T <= 1024
 
-  model = GPT(GPTConfig(n_layer=12, n_head=12, n_embd=768))
-  model.load_pretrained()
+  model = GPT(GPTConfig(n_layer=6, n_head=6, n_embd=384))
+  # model.load_pretrained()
 
   if args.gpus > 1:
     GPUS = tuple(f'{Device.DEFAULT}:{i}' for i in range(args.gpus))
